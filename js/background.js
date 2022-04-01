ee41440323df0bb31a559f077dd2342afa63085f
sieun43 <lsieun0229@gmail.com>
2022-04-01 15:28:02 +0900

    css 추가, 배경이미지 추가 및 기타사항 수정

--- js/background.js
+++ js/background.js
@@ -1,9 +1,10 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
 const choesenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${choesenImage}`;
+bgImage.classList.add("bgImg");
 
 document.body.appendChild(bgImage);
